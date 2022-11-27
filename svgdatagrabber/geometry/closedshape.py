d3818749810c1bc9613552ebb41eeabb6544d15a
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-11-27 21:10:54 +0800

    Update ClosedShape abstract methods

--- svgdatagrabber/geometry/closedshape.py
+++ svgdatagrabber/geometry/closedshape.py
@@ -15,18 +15,22 @@ class ClosedShape(GeometryBase, ABC):
         """Check if a point or shape is inside the shape."""
         raise NotImplementedError
 
+    @property
     def area(self) -> float:
         """Return the area of the shape."""
         raise NotImplementedError
 
+    @property
     def perimeter(self) -> float:
         """Return the perimeter of the shape."""
         raise NotImplementedError
 
+    @property
     def centroid(self) -> Point:
         """Return the centroid of the shape."""
         raise NotImplementedError
 
+    @property
     def bounding(self) -> Tuple[Point, Point]:
         """Return the bounding box of the shape."""
         raise NotImplementedError
