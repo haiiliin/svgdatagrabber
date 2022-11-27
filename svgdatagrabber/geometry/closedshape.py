8903bf76832fd8c7373b40f5f6948583eef18caa
Hailin Wang <hailin.wang@connect.polyu.hk>
2022-11-27 12:37:50 +0800

    Add ClosedShape class

---
+++ svgdatagrabber/geometry/closedshape.py
@@ -0,0 +1,31 @@
+from abc import ABC
+
+from .geometrybase import GeometryBase
+from .point import Point
+
+
+class ClosedShape(GeometryBase, ABC):
+    """Base class for closed shapes."""
+
+    def __eq__(self, other: "ClosedShape") -> bool:
+        raise NotImplementedError
+
+    def __contains__(self, item: GeometryBase) -> bool:
+        """Check if a point or shape is inside the shape."""
+        raise NotImplementedError
+
+    def area(self) -> float:
+        """Return the area of the shape."""
+        raise NotImplementedError
+
+    def perimeter(self) -> float:
+        """Return the perimeter of the shape."""
+        raise NotImplementedError
+    
+    def centroid(self) -> Point:
+        """Return the centroid of the shape."""
+        raise NotImplementedError
+
+    def bounding_box(self) -> tuple[Point, Point]:
+        """Return the bounding box of the shape."""
+        raise NotImplementedError
--- svgdatagrabber/geometry/geometrybase.py
+++ svgdatagrabber/geometry/geometrybase.py
@@ -1,9 +1,14 @@
-class GeometryBase:
+from abc import ABC
+
+
+class GeometryBase(ABC):
     #: Tolerance for equality.
     tolerance = 1e-6
 
     def __eq__(self, other: "GeometryBase") -> bool:
+        """Check if two geometries are equal."""
         raise NotImplementedError
 
     def __ne__(self, other: "GeometryBase") -> bool:
+        """Check if two geometries are not equal."""
         return not self.__eq__(other)
