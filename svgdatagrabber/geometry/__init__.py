a9aa0c4ed91acce5297aec123930958356b9e2d9
Hailin-Wang <hailin.wang@connect.polyu.hk>
2022-11-28 10:48:34 +0800

    Update __init__.py

--- svgdatagrabber/geometry/__init__.py
+++ svgdatagrabber/geometry/__init__.py
@@ -1,3 +1,18 @@
+from .circle import Circle
+from .closedshape import ClosedShape
+from .ellipse import Ellipse
 from .geometrybase import GeometryBase
-from .line import Line, Segment, Ray, ExtendedSegment, ExtendedRay
-from .point import Point, Vector
+from .line import Line, Segment, Ray, ExtendedSegment, ExtendedRay, LineBase, StraightLineBase, CurveLineBase
+from .point import Point, Vector, PointType
+from .polygon import Polygon
+from .quadrilateral import (
+    Quadrilateral,
+    Trapezoid,
+    Parallelogram,
+    Rectangle,
+    Square,
+    Kite,
+    RightKite,
+    IsoscelesTrapezoid,
+)
+from .triangle import Triangle
