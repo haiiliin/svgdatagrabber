f71c773c4e4a8c15a1b58c7ddf461398a24d53ec
Hailin Wang <wanghailin@tongji.edu.cn>
2024-12-06 13:23:16 +0800

    Update setuptools-scm configuration

--- svgdatagrabber/version.py
+++ svgdatagrabber/version.py
@@ -1,30 +1,25 @@
-from pathlib import Path
+from __future__ import annotations
 
-try:
-    from ._version import version as _default_version
-except ImportError:
-    _default_version = "0.0.1"
+from pathlib import Path
+from re import error as RegexError  # noqa
 
 
 def _get_version():
-    """Return the version string used for __version__."""
-    # Only shell out to a git subprocess if really needed, and not on a
-    # shallow clone, such as those used by CI, as the latter would trigger
-    # a warning from setuptools_scm.
     root = Path(__file__).resolve().parents[1]
     if (root / ".git").exists() and not (root / ".git/shallow").exists():
         try:
-            import setuptools_scm
+            import setuptools_scm  # noqa
 
-            return setuptools_scm.get_version(
-                root=str(root),
-                version_scheme="post-release",
-                fallback_version=_default_version,
-            )
-        except (ImportError, LookupError, Exception):
-            return _default_version
-    else:  # Get the version from the _version.py setuptools_scm file.
-        return _default_version
+            return setuptools_scm.get_version(root=str(root))
+        except (ImportError, RegexError, LookupError):
+            pass
+    try:
+        from ._version import version
+
+        return version
+    except ImportError:
+        return "0.0.0-unknown"
 
 
-__version__ = _get_version()
+__version__: str = _get_version()
+__semver__: str = __version__.split("+")[0]
